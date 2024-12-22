155be6fa26b9d84555b5049f4037673e85ae76ce
SushimRupakheti <sushimrupakheti120@gmail.com>
2024-12-22 23:10:34 +0545

    login page updated

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggle = document.getElementById('toggle');
-  
-    toggle.addEventListener('change', () => {
-      const forms = document.querySelector('.forms');
-      if (toggle.checked) {
-        forms.style.transform = 'translateX(-100%)';
-      } else {
-        forms.style.transform = 'translateX(0)';
-      }
-    });
-  });
-  
-
-
 let currentIndex = 0;
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length;
 
-function showSlide() {
-  slides.forEach((slide, index) => {
-    slide.classList.remove('active');
-    if (index === currentIndex) {
-      slide.classList.add('active');
-    }
-  });
-
-  currentIndex = (currentIndex + 1) % totalSlides;
+function changeImage(direction) {
+  const images = document.querySelectorAll(".slider img");
+  images[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + direction + images.length) % images.length;
+  images[currentIndex].classList.add("active");
 }
 
-setInterval(showSlide, 3000); // Change image every 3 seconds
